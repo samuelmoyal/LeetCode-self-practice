e2e757d953355ecfb9426163fe7e5cc98e01afcc
Samuel Moyal <134685124+samuelmoyal@users.noreply.github.com>
2025-11-08 22:39:12 -0500

    Time: 0 ms (100.00%) | Memory: 46.1 MB (19.84%) - LeetSync

--- LinkedList/Easy/2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ LinkedList/Easy/2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,39 +1,19 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        if len(mat)>0:
-            seen=set()
-            cands=[(0,0)]
-            while len(cands)>0:
-                print(cands)
-                test=cands.pop()
-                i,j=test
-                if i+1<len(mat) and (i+1,j) not in seen:
-                    if mat[i+1][j]>mat[i][j]:
-                        cands.append((i+1,j))
-                        seen.add((i,j))
-                    else:
-                        seen.add((i+1,j))
-                if i-1>=0 and (i-1,j) not in seen:
-                    if mat[i-1][j]>mat[i][j]:
-                        cands.append((i-1,j))
-                        seen.add((i,j))
-                    else:
-                        seen.add((i-1,j))
-                if j+1<len(mat[0]) and (i,j+1) not in seen:
-                    if mat[i][j+1]>mat[i][j]:
-                        cands.append((i,j+1))
-                        seen.add((i,j))
-                    else:
-                        seen.add((i,j+1))
-                if j-1>=0 and (i,j-1) not in seen:
-                    if mat[i][j-1]>mat[i][j]:
-                        cands.append((i,j-1))
-                        seen.add((i,j))
-                    else:
-                        seen.add((i,j-1))
-                if (i,j) not in seen:
-                    return [i,j]
-        else:
-            return O 
+        m, n = len(mat), len(mat[0])
+        left, right = 0, n - 1
         
+        while left <= right:
+            mid = (left + right) // 2
+            max_row = max(range(m), key=lambda i: mat[i][mid])
+            
+            left_bigger = mid > 0 and mat[max_row][mid - 1] > mat[max_row][mid]
+            right_bigger = mid < n - 1 and mat[max_row][mid + 1] > mat[max_row][mid]
+            
+            if not left_bigger and not right_bigger:
+                return [max_row, mid]  # pic trouvé
+            elif left_bigger:
+                right = mid - 1
+            else:
+                left = mid + 1
         
